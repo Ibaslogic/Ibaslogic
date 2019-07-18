f63081c8b33d08d0052b128f835c50f67f9b68a8
ibaslogic <ibaskunle@gmail.com>
2019-07-18 18:15:14 +0100

    expired header config

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,8 +55,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ibaslogic`,
+        short_name: `ibas`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -67,5 +67,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify-headers`, // make sure to put last in the array
   ],
 }
