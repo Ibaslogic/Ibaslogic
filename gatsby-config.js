153002caca4fe7d8989b3eb7230c2e0994721e34
ibaslogic <ibaskunle@gmail.com>
2019-07-18 14:02:54 +0100

    favicon done

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `src/images/logo_favicon.svg`, // This path is relative to the root of the site.
+        icon: `src/images/ib_icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
