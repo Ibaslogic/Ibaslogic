04107988b23508c06569804ec741a2541a6f82cd
ibaslogic <ibaskunle@gmail.com>
2019-11-03 20:07:26 +0100

    layout styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React from "react" 
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 
