ea522f6966b007eeb19608f3fbf10e5d7159c518
ibaslogic <ibaskunle@gmail.com>
2020-03-01 17:32:25 +0100

    scroll top styled

--- src/components/BlogPage/scrollTop.js
+++ src/components/BlogPage/scrollTop.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import { FaChevronUp } from "react-icons/fa"
 import { Link } from "react-scroll"
@@ -6,6 +6,30 @@ import { Link } from "react-scroll"
 import shareStyles from "../../templates/blogpage.module.scss"
 
 const ScrollTop = () => {
+  const [showScrollTop, useshowScrollTop] = useState(false)
+
+  const onScroll = () => {
+    if (window.pageYOffset > 3000) {
+      useshowScrollTop(true)
+      // this.setState({
+      //   showSocialShare: true,
+      // })
+    } else {
+      useshowScrollTop(false)
+      // this.setState({
+      //   showSocialShare: false,
+      // })
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll)
+  }, [])
+
+  // componentDidMount() {
+  //   window.addEventListener("scroll", this.onScroll)
+  // }
+
   return (
     <Link
       //activeClass="active"
@@ -14,10 +38,9 @@ const ScrollTop = () => {
       smooth={true}
       offset={-64}
       duration={500}
-      className={shareStyles.scrollTop}
+      className={showScrollTop ? `${shareStyles.scrollTop}` : ``}
     >
-      <FaChevronUp /> Back <br />
-      to top
+      <FaChevronUp /> Top
     </Link>
   )
 }
