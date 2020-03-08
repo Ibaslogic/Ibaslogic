c5fa0b2e075618cd59d06a51859357bb67d8f8eb
ibaslogic <ibaskunle@gmail.com>
2020-03-09 00:44:55 +0100

    click outside mobile nav

--- src/components/about/aboutHero.js
+++ src/components/about/aboutHero.js
@@ -11,15 +11,15 @@ const AboutHero = () => {
       >
         <div className={aboutHeroStyles.bannerContent}>
           <h1>
-            Want to build a modern <span class={aboutHeroStyles.nl}></span> web
-            project?
+            Want to build a modern <span className={aboutHeroStyles.nl}></span>{" "}
+            web project?
             {/* <span>help you improve </span>
             your <span class={aboutHeroStyles.nl}></span> Dev Knowledge */}
           </h1>
           <h3 className={aboutHeroStyles.description}>
             Then you're in the right place. You will get to learn how to apply
-            the tools <span class={aboutHeroStyles.nl}></span>and the strategy
-            needed for your next project.
+            the tools <span className={aboutHeroStyles.nl}></span>and the
+            strategy needed for your next project.
             {/* Enter your email below to get access to my web development
             (practical)<span class={aboutHeroStyles.nl}></span> tips and tricks. */}
           </h3>
--- src/components/globals/header.js
+++ src/components/globals/header.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 import { Link } from "gatsby"
 //import logo from "../../images/ibaslogic_logo.svg"
@@ -7,100 +7,108 @@ import logo from "../../images/ibaslogic4.png"
 import headerStyles from "./header.module.scss"
 import Helmet from "react-helmet"
 
-export default class Header extends Component {
-  state = {
-    navbarOpen: false,
-    // css: "navbar",
-    links: [
-      {
-        id: 1,
-        path: "/",
-        text: "home",
-      },
-      {
-        id: 2,
-        path: "/blog/",
-        text: "blog",
-      },
-      {
-        id: 3,
-        path: "/contact/",
-        text: "contact",
-      },
-      {
-        id: 4,
-        path: "/about/",
-        text: "about",
-      },
-    ],
+const Header = () => {
+  const node = useRef()
+  const [navbarOpen, setNavbarOpen] = useState(false)
+  const [links] = useState([
+    {
+      id: 1,
+      path: "/",
+      text: "home",
+    },
+    {
+      id: 2,
+      path: "/blog/",
+      text: "blog",
+    },
+    {
+      id: 3,
+      path: "/contact/",
+      text: "contact",
+    },
+    {
+      id: 4,
+      path: "/about/",
+      text: "about",
+    },
+  ])
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen)
   }
 
-  handleToggle = () => {
-    //console.log("clicked")
-    this.setState({
-      navbarOpen: !this.state.navbarOpen,
-    })
+  const handleClick = e => {
+    if (node.current.contains(e.target)) {
+      return
+    }
+    setNavbarOpen(false)
   }
 
-  render() {
-    return (
-      <header className={headerStyles.container}>
-        <Helmet>
-          <body
-            className={this.state.navbarOpen ? `${headerStyles.noScroll}` : ``}
-          />
-        </Helmet>
-        <div className={headerStyles.content}>
-          <div className={headerStyles.brand}>
-            <Link to="/" className={headerStyles.brandLink}>
-              <img src={logo} alt="ibaslogic logo" />
-            </Link>
-          </div>
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleClick) // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClick)
+    }
+  }, [])
 
-          <nav className={`${headerStyles.navbar}`}>
-            <div
-              className={
-                this.state.navbarOpen
-                  ? `${headerStyles.menuBtn} ${headerStyles.close}`
-                  : `${headerStyles.menuBtn}`
-              } // add close class
-              onClick={this.handleToggle}
-            >
-              <div className={headerStyles.btnLine}></div>
-              <div className={headerStyles.btnLine}></div>
-              <div className={headerStyles.btnLine}></div>
-            </div>
-            <ul // add show
-              className={
-                this.state.navbarOpen
-                  ? `${headerStyles.menuNav} ${headerStyles.showNav}`
-                  : `${headerStyles.menuNav}`
-              }
-            >
-              {this.state.links.map(link => {
-                return (
-                  <li key={link.id} className={headerStyles.navItem}>
-                    <Link
-                      to={link.path}
-                      className={headerStyles.navLink}
-                      activeClassName={headerStyles.activeNavItem}
-                    >
-                      {link.text}
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
-          </nav>
+  return (
+    <header className={headerStyles.container}>
+      <Helmet>
+        <body className={navbarOpen ? `${headerStyles.noScroll}` : ``} />
+      </Helmet>
+      <div className={headerStyles.content}>
+        <div className={headerStyles.brand}>
+          <Link to="/" className={headerStyles.brandLink}>
+            <img src={logo} alt="ibaslogic logo" />
+          </Link>
         </div>
-        <div
-          className={
-            this.state.navbarOpen
-              ? `${headerStyles.sidebarOverlay} ${headerStyles.showOverlay}`
-              : `${headerStyles.sidebarOverlay}`
-          }
-        ></div>
-      </header>
-    )
-  }
+
+        <nav ref={node} className={`${headerStyles.navbar}`}>
+          <div
+            className={
+              navbarOpen
+                ? `${headerStyles.menuBtn} ${headerStyles.close}`
+                : `${headerStyles.menuBtn}`
+            } // add close class
+            onClick={handleToggle}
+          >
+            <div className={headerStyles.btnLine}></div>
+            <div className={headerStyles.btnLine}></div>
+            <div className={headerStyles.btnLine}></div>
+          </div>
+          <ul // add show
+            className={
+              navbarOpen
+                ? `${headerStyles.menuNav} ${headerStyles.showNav}`
+                : `${headerStyles.menuNav}`
+            }
+          >
+            {links.map(link => {
+              return (
+                <li key={link.id} className={headerStyles.navItem}>
+                  <Link
+                    to={link.path}
+                    className={headerStyles.navLink}
+                    activeClassName={headerStyles.activeNavItem}
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </div>
+      <div
+        className={
+          navbarOpen
+            ? `${headerStyles.sidebarOverlay} ${headerStyles.showOverlay}`
+            : `${headerStyles.sidebarOverlay}`
+        }
+      ></div>
+    </header>
+  )
 }
+
+export default Header
