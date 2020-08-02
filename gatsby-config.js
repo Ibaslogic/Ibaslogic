1a5ccbc8ca9490ba2da49b849c696c83f4bfd26b
ibaslogic <ibaskunle@gmail.com>
2020-08-02 12:02:09 +0100

    lato fonts added

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,17 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Lato`,
+            variants: [`300`, `400`, `700`]
+          },
+        ],
+      },
+    },
+    {
       resolve: "gatsby-plugin-use-dark-mode",
       options: {
         classNameDark: "dark-mode",
