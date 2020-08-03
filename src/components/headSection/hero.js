352562d5c04b31895db2cd8eab4cfc097cad0b2e
ibaslogic <ibaskunle@gmail.com>
2020-08-03 15:46:30 +0100

    speed optimizatioin

--- src/components/headSection/hero.js
+++ src/components/headSection/hero.js
@@ -9,12 +9,14 @@ const Hero = () => {
     <div className={`hero__container ${heroStyles.homePageHero}`}>
       <div className={heroStyles.heroContent}>
         <h1 className={heroStyles.title}>
-          Web Dev Articles for the <span className={heroStyles.nlt}></span>
+          Web Dev Articles for the
           Self-Starter
+          {/* <span className={heroStyles.nlt}></span> */}
         </h1>
         <p className={heroStyles.description}>
-          Connect the dots (the latest web technologies) and complete a{" "}
-          <span className={heroStyles.nl}></span>real-world project.
+          Hey! Are you struggling connecting web technologies to form a real-world project? Here, you'll learn in a project-based method.
+          {/* Connect the dots (the latest web technologies) and complete a{" "}
+          <span className={heroStyles.nl}></span>real-world project. */}
         </p>
 
         <div className={heroStyles.heroButtonContainer}>
