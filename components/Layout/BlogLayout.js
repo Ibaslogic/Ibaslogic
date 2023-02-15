26e6e52dda1c87ac2c1ccdae5660e20f9229dcca
ibaslogic <ibaskunle@gmail.com>
2023-02-15 14:28:42 +0100

    Fix the post reading time

--- components/Layout/BlogLayout.js
+++ components/Layout/BlogLayout.js
@@ -66,7 +66,6 @@ const BlogLayout = ({ children, frontmatter }) => {
       };
 
   const { name, link, avatar, handle } = authorInfo;
-  // console.log(readingTime);
   const actualReadingTime = Math.floor(readingTime.minutes / 2);
   return (
     <Layout
@@ -86,7 +85,6 @@ const BlogLayout = ({ children, frontmatter }) => {
                 <p>{format(parseISO(date), 'MMMM dd, yyyy')}</p>
                 <span>{' . '}</span>
                 <span>{`${actualReadingTime} min read`}</span>
-                <span className="hidden">{`${readingTime.text}`}</span>
               </div>
 
               <h1 className="text-3xl font-bold md:text-[2.5rem] md:leading-[1.25] text-black mb-[0.5em] lg:mt-8">
